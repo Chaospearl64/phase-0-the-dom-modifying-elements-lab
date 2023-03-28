8e5245305079e87d1de6d825e177238bb3f999a9
Cody Cogbill <codeman24@gmail.com>
2023-03-28 10:19:01 -0600

    dom manipulation lab done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here!
+// const element = document.createElement("div");
+
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement("li");
+//     li.textContent = (i + 1).toString();
+//     ul.append(li);
+//   }
+//   element.append(ul);
+
+// element.className = "pet-listing dog";
+
+// element.classList.remove("dog");
+// element.classList.add("cat", "sale");
+
+// const ul = document.getElementsByTagName("ul")[0];
+// const secondChild = ul.querySelector("li:nth-child(2)");
+// ul.removeChild(secondChild);
+
+
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Cody is the champion"
